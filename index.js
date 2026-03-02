001151f614476cea44820e325d59eb9f6c78a15c
prantaroy11 <prantaroy10@gmail.com>
2026-03-02 10:51:24 +0530

    Freature:Basic app setup

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express=require('express');
-const bodyParser=require('body-parser');
 const env=require('dotenv');
 env.config();
+const mongoose=require('mongoose');
 
 
 const app=express();
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true}));
 
 
-app.listen(3000,()=>{
+app.listen(3000,async()=>{
     console.log("Server is running on port 3000");
+
+   await mongoose.connect(process.env.DB_URL);
+   console.log("successfully connect mongo");
 })
