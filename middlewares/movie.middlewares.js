9a7611f25b58839f8078bc8fd3d20d637fc4cd39
prantaroy11 <prantaroy10@gmail.com>
2026-03-11 02:06:55 +0530

    Fix:Addes codes for middlewares of movies api

--- middlewares/movie.middlewares.js
+++ middlewares/movie.middlewares.js
@@ -1,3 +1,4 @@
+const {STATUS}=require('../utils/constants');
 
 const badRequestBody={
     success:false,
@@ -17,37 +18,37 @@ const badRequestBody={
 const validateMovieCreateRequest=async(req,res,next)=>{
     if(!req.body.name){
         badRequestBody.err="Name of the movie is required";
-        return res.status(400).json(badRequestBody);
+        return res.status(STATUS.BAD_REQUEST).json(badRequestBody);
     }
 
     if(!req.body.description){
         badRequestBody.err="Description of the movie is required";
-        return res.status(400).json(badRequestBody);
+        return res.status(STATUS.BAD_REQUEST).json(badRequestBody);
     }
 
     if(!req.body.casts || !Array.isArray(req.body.casts) || req.body.casts.length===0){
         badRequestBody.err="Casts of the movie is required";
-        return res.status(400).json(badRequestBody);
+        return res.status(STATUS.BAD_REQUEST).json(badRequestBody);
     }
 
     if(!req.body.trailerUrl){
         badRequestBody.err="Trailer URL of the movie is required";
-        return res.status(400).json(badRequestBody);
+        return res.status(STATUS.BAD_REQUEST).json(badRequestBody);
     }
     
     if(!req.body.releaseDate){
         badRequestBody.err="Release date of the movie is required";
-        return res.status(400).json(badRequestBody);
+        return res.status(STATUS.BAD_REQUEST).json(badRequestBody);
     }
 
     if(!req.body.director){
         badRequestBody.err="Director name of the movie is required";
-        return res.status(400).json(badRequestBody);
+        return res.status(STATUS.BAD_REQUEST).json(badRequestBody);
     }
 
     if(!req.body.releaseStatus){
         badRequestBody.err="Release status of the movie is required";
-        return res.status(400).json(badRequestBody);
+        return res.status(STATUS.BAD_REQUEST).json(badRequestBody);
     }
 
     next();
