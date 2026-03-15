256c3a1f2671436bc1c61518bccb06c5d3583aad
prantaroy11 <prantaroy10@gmail.com>
2026-03-15 20:30:53 +0530

    Fix:import movieService in booking middleware

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/',(req,res)=>{
     res.send('Home');
 });
 
-app.listen(3000,async()=>{
+app.listen(process.env.PORT,async()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
     try{
         if(process.env.NODE_ENV=='production'){
