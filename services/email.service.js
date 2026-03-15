689cc0ce489dd8aa7750f7635bd012d11b27e178
prantaroy11 <prantaroy10@gmail.com>
2026-03-15 21:12:35 +0530

    Fix:Added try catch block in emailService

--- services/email.service.js
+++ services/email.service.js
@@ -1,13 +1,20 @@
 const axios=require('axios');
 const User=require('../models/user.model');
+const { response } = require('express');
 
 const sendMail= async(subject,id,content)=>{
-    const user=await User.findById(id);
-    await axios.post(process.env.NOTI_SERVICE + '/notiservice/api/v1/notifications',{
-        subject:subject,
-        recepientEmails:[user.email],
-        content:content
-    });
+    try{
+        const user=await User.findById(id);
+            await axios.post(process.env.NOTI_SERVICE + '/notiservice/api/v1/notifications',{
+            subject:subject,
+            recepientEmails:[user.email],
+            content:content
+        });
+
+        console.log("Notification service response",response.data);
+    }catch(err){
+        console.error("Notification service error:", err.message);
+    }
 }
 
 module.exports=sendMail;
