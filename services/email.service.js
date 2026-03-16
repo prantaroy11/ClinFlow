bbfa9206750fbb89052a3ec919e19d9fece8ecff
prantaroy11 <prantaroy10@gmail.com>
2026-03-16 15:52:54 +0530

    Fix:Try to loggs mail issue

--- services/email.service.js
+++ services/email.service.js
@@ -2,12 +2,16 @@ const axios=require('axios');
 const User=require('../models/user.model');
 
 const sendMail= async(subject,id,content)=>{
-    const user=await User.findById(id);
-    await axios.post(process.env.NOTI_SERVICE + '/notiservice/api/v1/notifications',{
-        subject:subject,
-        recepientEmails:[user.email],
-        content:content
-    });
+    try{
+        const user=await User.findById(id);
+            await axios.post(process.env.NOTI_SERVICE + '/notiservice/api/v1/notifications',{
+            subject:subject,
+            recepientEmails:[user.email],
+            content:content
+        });
+    }catch(err){
+        console.log("Failed to send notification request:", error.message);
+    }
 }
 
 module.exports=sendMail;
